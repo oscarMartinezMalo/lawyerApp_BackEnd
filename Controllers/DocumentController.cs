ec73ac58778f77c0bba03f95883766bef158ebed
Oscar Martinez Malo <omartinez@onehomecaresolutions.net>
2021-10-20 01:03:42 -0400

    Completing the file upload

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +12,6 @@ namespace LawyerApp.Controllers
     [ApiController]
     public class DocumentController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
 
         [HttpGet]
         [ActionName("")]
@@ -22,6 +19,29 @@ namespace LawyerApp.Controllers
         {
             string filePath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "firstForm.docx");
             return File(System.IO.File.ReadAllBytes(filePath), "application/octet-stream", "firstForm.docx");
+        }
+
+        [HttpPost]
+        [ActionName("")]
+        public IActionResult Post(IFormFile document)
+        {
+            //var httpRequest = HttpContext.Request.Form;
+            //if (httpRequest.Files.Count < 1) return BadRequest("You have to attach a document");
+
+            string uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "documents");
+            var uniqueFileName = DateTime.Now.ToString("yyyyMMddTHH-mm-ssZ")  + "_" + Guid.NewGuid().ToString() + "_" + document.FileName;
+            string fileAddress = Path.Combine(uploadFolder, uniqueFileName);
+
+            document.CopyTo(new FileStream(fileAddress, FileMode.Create));
+
+            return Ok("File successfully submited!!!");
+        }
+
+        [HttpPost]
+        [ActionName("uploadFiles")]
+        public IActionResult Post(IList<IFormFile> document)
+        {
+            return Ok();
         }
     }
 }
---
+++ Services/DocumentService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LawyerApp.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        public void Upload(IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
